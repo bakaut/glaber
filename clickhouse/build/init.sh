7fdefd72a1855495c3da3ff0ccb171005c0eaa7c
Nikolay Lebedev <fifo.mail@gmail.com>
2020-02-24 14:03:54 +0300

    clickhouse fix

--- clickhouse/build/init.sh
+++ clickhouse/build/init.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e 
 
-clickhouse-client --user default --password zabbix -q "SELECT * FROM zabbix.history limit 1;" || \
-clickhouse-client --user default --password zabbix -n <<-EOSQL
-  CREATE DATABASE zabbix;
-  CREATE TABLE zabbix.history ( 
+ZBX_CH_DB=${ZBX_CH_DB:-"zabbix"}
+ZBX_CH_USER=${ZBX_CH_USER:-"default"}
+ZBX_CH_PASS=${ZBX_CH_PASS:-"zabbix"}
+
+clickhouse-client --user ${ZBX_CH_USER} --password ${ZBX_CH_PASS} -q "SELECT * FROM ${ZBX_CH_DB}.history limit 1;" || \
+clickhouse-client --user ${ZBX_CH_USER} --password ${ZBX_CH_PASS} -n <<-EOSQL
+  CREATE DATABASE IF NOT EXISTS ${ZBX_CH_DB};
+  CREATE TABLE IF NOT EXISTS ${ZBX_CH_DB}.history ( 
       itemid UInt64,  
       clock DateTime Codec(DoubleDelta, LZ4),  
       ns UInt32, 
@@ -12,11 +16,29 @@ clickhouse-client --user default --password zabbix -n <<-EOSQL
       value_dbl Float64 Codec(Gorilla, LZ4), 
       value_str LowCardinality(String) Codec(LZ4)
       ) ENGINE = MergeTree() PARTITION BY toYYYYMMDD(clock) ORDER BY (itemid, clock) SETTINGS index_granularity=8192;
-    CREATE TABLE zabbix.history_buffer (day Date,  
+    CREATE TABLE IF NOT EXISTS ${ZBX_CH_DB}.history_buffer (day Date,  
                                     itemid UInt64,  
                                     clock DateTime,  
                                     ns UInt32,  
                                     value Int64,  
                                     value_dbl Float64,  
-                                    value_str String ) ENGINE = Buffer(zabbix, history, 8, 30, 60, 9000, 60000, 256000, 256000000);
+                                    value_str String ) ENGINE = Buffer(${ZBX_CH_DB}, history, 8, 30, 60, 9000, 60000, 256000, 256000000);
+EOSQL || \
+clickhouse-client --user ${ZBX_CH_USER} --password ${ZBX_CH_PASS} -n <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS ${ZBX_CH_DB};
+    CREATE TABLE IF NOT EXISTS ${ZBX_CH_DB}.history ( day Date,  
+                                    itemid UInt64,  
+                                    clock DateTime,  
+                                    ns UInt32, 
+                                    value Int64,  
+                                    value_dbl Float64,  
+                                    value_str String 
+                                ) ENGINE = MergeTree(day, (itemid, clock), 8192);
+    CREATE TABLE IF NOT EXISTS ${ZBX_CH_DB}.history_buffer (day Date,  
+                                    itemid UInt64,  
+                                    clock DateTime,  
+                                    ns UInt32,  
+                                    value Int64,  
+                                    value_dbl Float64,  
+                                    value_str String ) ENGINE = Buffer(${ZBX_CH_DB}, history, 8, 30, 60, 9000, 60000, 256000, 256000000);
 EOSQL
