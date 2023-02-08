c82cc52b7c3dc224040ca5ffe396c9a5429bdecd
Nikolay Lebedev <fifo.mail@gmail.com>
2023-02-08 22:47:53 +0100

    change source of hitory.sql to glaber repo

--- clickhouse/init.sh
+++ clickhouse/init.sh
@@ -3,11 +3,11 @@ set -e
 
 if [ ! -f database.exist ]; then
 
-  wget https://gitlab.com/mikler/glaber/-/raw/${GLABER_BRANCH}/database/clickhouse/history.sql -o /root/history.sql
+  wget -q https://gitlab.com/mikler/glaber/-/raw/${GLABER_BRANCH}/database/clickhouse/history.sql
 
   sed -i -e "s/glaber/${ZBX_CH_DB}/g" \
          -e "s/6 MONTH/${ZBX_CH_RETENTION}/g" \
-         /root/history.sql
+         history.sql
   
 # File users.xml should be defined before init.sh script
 # Redefine with .env file works with .glaber.sh script at prebuild stage
@@ -15,7 +15,7 @@ if [ ! -f database.exist ]; then
 
   clickhouse-client \
     --user ${ZBX_CH_USER} --password ${ZBX_CH_PASS} \
-    --multiquery < /root/history.sql
+    --multiquery < history.sql
 
   touch database.exist
 fi
